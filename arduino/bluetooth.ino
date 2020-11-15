7692a6f77e072cea6a6bafbc59e276c234bc1cb5
Umbra <pixzel.mcyt@gmail.com>
2020-11-15 11:45:19 +0100

    update arduino code

--- arduino/bluetooth.ino
+++ arduino/bluetooth.ino
@@ -1,8 +1,8 @@
 const int mVolt = 3300;
 const int tmpPin = A0;
 const int phPin = A1;
-const int otherPin = A2;
-float tmpVal, phVal, otherVal = 0;
+const int waterPin = A2;
+float tmpVal, phVal, waterVal = 0;
 
 void setup() {
     Serial.begin(9600);
@@ -13,15 +13,21 @@ void loop() {
 
     //read input voltage from analog pin and calculate the value based on formula
     vin = analogRead(tmpPin);
-    tmpVal= vin*(mVolt/1024)-500)/10;
+    tmpVal= (vin*(mVolt/1024)-500)/10;
 
     vin = analogRead(phPin);
     phVal = vin;
 
-    vin = analogRead(otherPin);
-    otherVal = vin;
+    vin = analogRead(waterPin);
+    waterVal = vin;
 
     //write results to serial port (Nano 33 BLE serial is bluetooth)
     Serial.print("Example temperature reading: ");
     Serial.println(tmpVal, 5);
+    
+    Serial.print("Example PH reading: ");
+    Serial.println(phVal, 2);
+    
+    Serial.print("Example flood reading: ");
+    Serial.println(waterVal);
 }
