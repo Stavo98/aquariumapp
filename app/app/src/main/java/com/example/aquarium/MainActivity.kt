4a619497b435a2e6511f273c0fe16ed603f82458
Umbra <pixzel.mcyt@gmail.com>
2020-11-16 13:20:04 +0100

    add BLE functionality

--- app/app/src/main/java/com/example/aquarium/MainActivity.kt
+++ app/app/src/main/java/com/example/aquarium/MainActivity.kt
@@ -6,9 +6,17 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.content.Context
+import android.content.Intent
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCallback
+import android.bluetooth.BluetoothGattServer
+import android.bluetooth.BluetoothGattServerCallback
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,4 +43,42 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    private val bluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().bluetoothLeScanner
+    private var mScanning = false
+    private val handler = Handler()
+
+    // Stops scanning after 10 seconds.
+    private val SCAN_PERIOD: Long = 10000
+
+    fun connectBLE() {
+        // Initializes Bluetooth adapter.
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter
+
+        if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+    }
+
+    private fun scanLeDevice() {
+        if (!mScanning) { // Stops scanning after a pre-defined scan period.
+            handler.postDelayed({
+                mScanning = false
+                bluetoothLeScanner.stopScan(leScanCallback)
+            }, SCAN_PERIOD)
+            mScanning = true
+            bluetoothLeScanner.startScan(leScanCallback)
+        } else {
+            mScanning = false
+            bluetoothLeScanner.stopScan(leScanCallback)
+        }
+    }
+
+    fun close() {
+        bluetoothGatt?.close()
+        bluetoothGatt = null
+    }
+
 }
